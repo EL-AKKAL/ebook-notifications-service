f4940f72cf0d80c479e32fd261ac77ad6b1539a6
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-02-04 08:30:22 +0100

    feat : add api docs to main route

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,12 @@
 <?php
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Ebook Notifications Service is running.',
+        'api routes' => [
+            '/notifications' => 'Get all notifications',
+            '/notifications/{id}/read' => 'Mark a notification as read',
+            '/notifications/read-all' => 'Mark all notifications as read',
+        ]
+    ]);
+});
