73c35ca96610d52fd9fd4eb067328c00829cfad9
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-02-04 17:35:23 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+set -e
+
+cd /var/www/ebook-notifications-service
+
+git pull origin main
+
+composer install --no-dev --optimize-autoloader
+
+php artisan migrate --force
+
+php artisan optimize:
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+sudo systemctl reload php8.3-fpm
